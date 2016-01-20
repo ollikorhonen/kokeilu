2d02895531add2033c7134ae037302357d86179b
ollikorhonen <K1937@student.jamk.fi>
2016-01-20 18:33:36 +0200

    moi kokeilu

---
+++ kokeilu/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kokeilu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Terveppä terve");
+            Console.ReadLine();
+        }
+    }
+}
